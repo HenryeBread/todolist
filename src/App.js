3ee25846d1e0d9dba72ea358b9ec0f4abbd45c06
HenryeBread <80848233+HenryeBread@users.noreply.github.com>
2023-12-05 13:36:51 -0800

    Created the todo list app, deleted react files.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useEffect } from "react";
+import { NewForm } from './NewForm'
+import { TodoList } from './TodoList'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [todos, setTodos] = useState(() => {
+        const localValue = localStorage.getItem("ITEMS")
+        if (localValue == null) return []
+
+        return JSON.parse(localValue)
+    })
+
+    useEffect(() => {
+        localStorage.setItem("ITEMS", JSON.stringify(todos))
+    }, [todos])
+
+    function addTodo(title) {
+        setTodos(currentTodos => {
+            return [
+                ...currentTodos,
+                { id: crypto.randomUUID(), title, completed: false
+                },
+            ]
+        })
+    }
+
+    
+    function deleteTodo(id) {
+        setTodos(currentTodos => {
+            return currentTodos.filter(todo => todo.id !== id)
+        })
+    }
+
+    function toggleTodo(id, completed) {
+        setTodos(currentTodos => {
+            return currentTodos.map(todo => {
+                if (todo.id === id){
+                    return {...todo, completed}
+                }
+                return todo
+            })
+        })
+    }
+
+    return (
+        <>
+            <NewForm onSubmit={addTodo}/>
+            <h1 className="header">ToDo List</h1>
+            <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
+            
+        </>
+    )
 }
 
 export default App;
